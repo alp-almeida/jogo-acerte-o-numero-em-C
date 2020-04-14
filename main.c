f9fb9da8092e5f0d1aef321e2de4450e5b130d7d
André Luiz Pereira de Almeida <andre_almeida15@hotmail.com>
2020-04-14 16:45:31 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,25 @@
 /*
  * main.c
  * 
- * Copyright 2016 André Luiz Pereira de Almeida
+ * Copyright 2016 AndrÃ© Luiz Pereira de Almeida
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
  */
 
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
 int set;
 int count;
 char opt = 'n';
 
 void startSet(){
-	
 	srand((unsigned)time(NULL));
 	set = 1+ rand()%10;
 	count = 0;
@@ -54,13 +42,16 @@ int main(){
 	startSet();
 	while ( !game(num) && count < 3 ){
 	
-		printf("Digite um numero entre 00 e 09: ");
-		scanf("%d", &num);
+		while(!isdigit(num)){
+			printf("Digite um numero entre 00 e 09: ");
+			fflush(stdin);
+			scanf("%d", &num);
+		}
 		count++;
-		fflush(stdin);
 		
 		if(game(num)){
 			printf("Parabens voce acertou!\n");
+			break;
 		}else{
 			printf("Chance %d de 3 falhou!\t",count);
 		
@@ -81,6 +72,8 @@ int main(){
 		{
 			system("CLS");
 			main();
+		}elseif (opt == 'n'){
+			break;
 		}
 			
 	}while(opt != 's' && opt != 'n');
